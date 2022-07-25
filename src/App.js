51dbf9c8b5b83bdf12f15ecd103fa904ed4d1947
Bharath C Hegde <ac.bharath123@gmail.com>
2022-07-25 21:20:13 +0530

    Rendering list with passing index as key topic updated

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ function App() {
   const add = "Bharath";
   const app = "Welcome " + add;
   const lang = ["html","css","kotlin","javascript"];
-  const menus = ["Masaladosa","panipuri","gobi"];
+   const menus = ["Masaladosa","panipuri","gobi"];
 
+   console.log(app);
   /*list rendering*/
-  for(var i=0; i<lang.length; i++){
-    console.log(lang[i]);
-  }
+ for(var i=0; i<lang.length; i++){
+  console.log(lang[i]);
+ }
   /*end list rendering*/
 
   /*conditional rendering*/
   function conditionalRendering(){
-  const menu = menus.length === 0?<p>List is empty</p>:menus.map(menuss => <li>{menuss}</li>)
-  return menu;
+    const menu = menus.length === 0?<p>List is empty</p>:menus.map((menuss,index) => <li key={index}>{menuss}</li>)
+    return menu;
   }
   /*end conditional rendering*/
 
-  console.log(app);
   console.log(lang[0]);
 
   return (
@@ -28,7 +28,7 @@ function App() {
   <button type="button" className="btn btn-danger">This Server Running with Bootstrap v.5.0.2</button>
    <p>{app}</p>
    <ul style={{textAlign:'left'}}>
-    {lang.map(langs => <li>{langs}</li>)}
+    {lang.map((langs,index) => <li key={index}>{langs}</li>)}
    </ul>
    <ul style={{textAlign:'left'}}>
    {conditionalRendering()}
