cc6d22fa474250150f4b379f1181163ef3471ec2
Bharath C Hegde <ac.bharath123@gmail.com>
2022-08-21 20:19:29 +0530

    learnt some concept

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -3,16 +3,16 @@ import React,{Component} from 'react';
 class Counter extends Component{
   state = {
     count:0
-   }
+  }
   render(){
     return(
-        <React.Fragment>
-        <button className="btn btn-danger mx-2">Decrement</button>
-        <span>{this.state.count}</span>
-        <button className="btn btn-warning mx-2">Increment</button>
-       </React.Fragment>
+      <React.Fragment>
+       <button className='btn btn-danger'>Increment</button>
+       <span>{this.state.count}</span>
+       <button className='btn btn-warning'>Decrement</button>
+      </React.Fragment>
     );
-  }
+    }
 }
 
 export default Counter;
